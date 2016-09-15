9bb05522baa23da8617f76316efcbacbb1515fe6
Hugo Courtial <hugo@courtial.me>
2016-09-15 23:33:02 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject ancap-generator "0.0.f*ckit"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [twitter-api "0.7.8"]
+                 [environ "1.0.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.12"]
+                 [clj-http "2.0.1"]
+                 [overtone/at-at "1.2.0"]]
+  :main ^:skip-aot ancap-generator.core
+  :plugins [[lein-environ "1.0.0"]])
